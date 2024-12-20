8692407bf8e2675765b45b053ef9a74e98632c60
RajnishPuri <rajnishpuri15@gmail.com>
2024-12-20 11:56:26 +0530

    Input fields added in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./Pages/Home"
+import PilotLogin from "./Pages/PilotLogin"
+import UserLogin from "./Pages/UserLogin"
+import UserSignup from "./Pages/UserSignup"
+import PilotSignup from "./Pages/PilotSignup"
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/userLogin" element={<UserLogin />} />
+        <Route path="/userSignup" element={<UserSignup />} />
+        <Route path="/pilotLogin" element={<PilotLogin />} />
+        <Route path="/pilotSignup" element={<PilotSignup />} />
       </Routes>
     </div>
   )
--- Frontend/src/Pages/UserLogin.jsx
+++ Frontend/src/Pages/UserLogin.jsx
@@ -1,7 +1,18 @@
+import { Mail, Lock } from 'lucide-react'
+import { useState } from 'react'
+
+import Input from "../components/Input"
 const UserLogin = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     return (
-        <div>
-            UserLogin
+        <div className=' w-screen h-screen py-10'>
+            <div className=' flex flex-col justify-center items-center '>
+                <h3 className='text-xl font-semibold pb-3'>Whats Your Email!</h3>
+                <Input icon={Mail} type="email" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <Input icon={Lock} type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <button className='w-3/4 bg-black text-white py-3 rounded mt-2 '>Login</button>
+            </div>
         </div>
     )
 }
