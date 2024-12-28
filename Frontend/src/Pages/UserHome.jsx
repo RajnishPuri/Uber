14507d6885ea2870dd573e79927a184cffb3fe54
Rajnish Puri <rajnishpuri15@gmail.com>
2024-12-28 23:37:29 +0530

    Added frontend of user

--- Frontend/src/Pages/UserHome.jsx
+++ Frontend/src/Pages/UserHome.jsx
@@ -1,6 +1,16 @@
 import Uber_Logo_Black from '/Uber_Logo_Black.png'
+import { useState } from 'react';
+import { ChevronDown } from 'lucide-react'
+import { ChevronUp } from 'lucide-react'
+import { MapPin } from 'lucide-react';
+import LocationSearchPanel from '../components/LocationSearchPanel';
 const UserHome = () => {
-    function fullSearchhandler() {
+    const [panel, setPanel] = useState(false);
+    const [pickup, setPickup] = useState('');
+    const [drop, setDrop] = useState('');
+
+    function formSubmitHandler(e) {
+        e.preventDefault();
 
     }
     return (
@@ -11,21 +21,41 @@ const UserHome = () => {
             </div>
             <div>
                 <div className='  h-screen absolute top-0 w-full flex flex-col justify-end'>
-                    <div className=' h-[30%] p-4 bg-white flex flex-col gap-4'>
-                        <h4 className='text-2xl font-semibold text-center'>Find a Trip</h4>
-                        <form className=' flex flex-col gap-2 w-full'>
-                            <input className=' bg-[#eee] px-8 py-2 text-base rounded-lg w-full' type="text" placeholder='Add Pickup Location' onClick={fullSearchhandler} />
-                            <div className='h-10 absolute w-1 bg-black translate-y-2/3 translate-x-3'></div>
-                            <input className=' bg-[#eee] px-8 py-2 text-base rounded-lg w-full' type="text" placeholder='Add Drop Location' onClick={fullSearchhandler} />
+                    <div className=' h-[25%] p-4 bg-white flex flex-col gap-4 justify-center'>
+                        <div className='flex justify-between items-center'>
+
+                            <h4 className='text-2xl font-semibold text-center'>Find a Trip</h4>
+
+                            {panel ?
+                                <ChevronDown onClick={() => {
+                                    setPanel(false);
+                                }} /> :
+                                null
+                            }
+                        </div>
+
+                        <form className=' flex flex-col gap-2 w-full ' onSubmit={(e) => {
+                            formSubmitHandler(e);
+                        }}>
+                            <input className=' bg-[#eee] px-8 py-2 text-base rounded-lg w-full' type="text" placeholder='Add Pickup Location' value={pickup} onClick={() => setPanel(true)} onChange={(e) => {
+                                setPickup(e.target.value);
+                            }} />
+                            <div className='h-10 absolute w-1 bg-[#4b4444ee] translate-y-2/3 translate-x-3'></div>
+                            <input className=' bg-[#eee] px-8 py-2 text-base rounded-lg w-full' type="text" placeholder='Add Drop Location' onClick={() => setPanel(true)} value={drop} onChange={(e) => {
+                                setDrop(e.target.value);
+                            }} />
                         </form>
                     </div>
-                    <div className=' bg-red-500 h-[0]'>
+                    {panel ? <div className=' bg-white h-[75%]'>
+                        <LocationSearchPanel />
+                    </div> : null}
 
-                    </div>
+
                 </div>
             </div>
-
         </div>
+
+
     )
 }
 
